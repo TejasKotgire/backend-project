dba84ec7e77bc323f66b4d16b7a9e75222c756b2
Tejas Kotgire <tejaskotgire@gmail.com>
2024-04-22 20:54:48 +0530

    add utils files

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ dotenv.config({
 
 
 connectDB()
+.then(() =>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+
+    // app.on("error", (err) => {
+    //     console.log("Error: ", err);
+    //     throw err;
+    // })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed: " + err);
+})
 
 
 
